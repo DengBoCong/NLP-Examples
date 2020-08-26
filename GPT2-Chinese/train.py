169e42c938f8f48e0b0cb6e971b0ec3f07f8f474
DBC <1210212670@qq.com>
2020-08-26 23:51:30 +0800

    GPT2-Chinese

---
+++ GPT2-Chinese/train.py
@@ -0,0 +1,12 @@
+import transformers
+import torch
+import os
+import json
+import random
+import numpy as np
+import argparse
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+from tqdm import tqdm
+from torch.nn import DataParallel
+from tokenizations.bpe_tokenizer import get_encoder
